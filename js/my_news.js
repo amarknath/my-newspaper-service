e58a24858d47d5aaa231acc6371db245fc819a88
Caley Dorsey <dorseycaley@gmail.com>
2016-08-24 16:07:14 -0400

    fixed layout for widths under 500 px for front page, made the type smaller on the desktop layout, state menu now expands the size of the full page

--- js/my_news.js
+++ js/my_news.js
@@ -1,4 +1,5 @@
 var acc = document.getElementsByClassName("accordion");
+var space = document.getElementsByClassName("whiteSpace");
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -7,3 +8,149 @@ for (i = 0; i < acc.length; i++) {
 		    this.nextElementSibling.classList.toggle("show");
 		}
 	}
+
+
+//var flip = 0;
+//$( acc ).click(function() {
+//  $( space ).toggle( flip++ % 2 === 0 );
+//});
+
+//$( acc ).click(function() {
+//  $( space ).toggle( "duration (default: 400)" );
+//});
+
+$( ".accordion.alabama" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.arizona" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.arkansas" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.california" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.colorado" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.delaware" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.florida" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.guam" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.indiana" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.iowa" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.kentucky" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.louisiana" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.maryland" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.michigan" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.minnesota" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.mississippi" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.missouri" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.montana" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.nevada" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.newjersey" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.newmexico" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.newyork" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+$( ".accordion.northcarolina" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.ohio" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.oregon" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.pennsylvania" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.southcarolina" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.southdakota" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.tennessee" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.texas" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.utah" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.vermont" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.virginia" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
+$( ".accordion.wisconsin" ).click(function() {
+  $( space ).toggle( "duration (default: 400)" );
+});
+
