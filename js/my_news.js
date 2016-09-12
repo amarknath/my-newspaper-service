682a78ac939d4da1215ed92dca8325884911218a
am@rn@th <amarnathk.k1@gmail.com>
2016-09-12 11:25:15 -0400

    logo@2xs images.

--- js/my_news.js
+++ js/my_news.js
@@ -1,14 +1,14 @@
-$.getJSON( "test.json", function( data ) {
-  document.getElementById('states').appendChild(createDOM(data));
-  dropStateList();
+$.getJSON("config.json", function(data) {
+    document.getElementById('states').appendChild(createDOM(data));
+    dropStateList();
 });
 
 function createDOM(data) {
-     var section = document.createElement('section');
-     section.className = 'state-menu';
-    $.each( data, function( key, val ) {
+    var section = document.createElement('section');
+    section.className = 'state-menu';
+    $.each(data, function(key, val) {
         var button = document.createElement('button');
-        button.className = 'accordion '+(key.toLowerCase()).replace(/\s/g, '');
+        button.className = 'accordion ' + (key.toLowerCase()).replace(/\s/g, '');
         button.appendChild(document.createTextNode(key.toUpperCase()));
         section.appendChild(button);
         section.appendChild(makeSublist(val));
@@ -17,40 +17,36 @@ function createDOM(data) {
 }
 
 function makeSublist(data) {
-  var div = document.createElement('div')
-  div.className = 'panel';
-  var  ul = div.appendChild(document.createElement('ul'));
-  for(var i = 0; i < data.length; i++) {
-  $.each( data[i], function( key, val ) {
-    var item = document.createElement('li');
-    item.className = 'publication-list-item';
-    var url = item.appendChild(document.createElement('a'));
-        url.appendChild(document.createTextNode(key));
-        url.setAttribute('href', 'publication.html?market='+key);
-        ul.appendChild(item);
-  }); 
-}
-  return div;
+    var div = document.createElement('div')
+    div.className = 'panel';
+    var ul = div.appendChild(document.createElement('ul'));
+    for (var i = 0; i < data.length; i++) {
+        $.each(data[i], function(key, val) {
+            var item = document.createElement('li');
+            item.className = 'publication-list-item';
+            var url = item.appendChild(document.createElement('a'));
+            url.appendChild(document.createTextNode(key));
+            url.setAttribute('href', 'publication.html?market=' + key);
+            ul.appendChild(item);
+        });
+    }
+    return div;
 }
 
 function dropStateList() {
-var acc = document.getElementsByClassName("accordion");
-var space = document.getElementsByClassName("whiteSpace");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-      
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+    var acc = document.getElementsByClassName("accordion");
+    var space = document.getElementsByClassName("whiteSpace");
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
     }
-  }
-
-var flip = 0;
-$( acc ).click(function() {
- $( space ).toggle( flip++ % 2 === 0 );
-});
-
-$( acc ).click(function() {
- $( space ).toggle( "duration (default: 400)" );
-});
+    var flip = 0;
+    $(acc).click(function() {
+        $(space).toggle(flip++ % 2 === 0);
+    });
+    $(acc).click(function() {
+        $(space).toggle("duration (default: 400)");
+    });
 }
--- js/publication.js
+++ js/publication.js
@@ -1,87 +1,77 @@
 var market = decodeURIComponent(location.search.split('market=')[1])
 
-if(market != undefined){
-  $.getJSON( "test.json", function( data ) {
+if (market != undefined) {
+    $.getJSON("config.json", function(data) {
 
-  $.each( data, function( key, val ) {
-  	for(var i = 0; i < val.length; i++) {
-  		$.each( val[i], function( key, val ) {
-  			if (market == key) {
-  				setParameters(val);
-  			}
-  		
-  		});	
-  	};
-  });	
-  
-});
+        $.each(data, function(key, val) {
+            for (var i = 0; i < val.length; i++) {
+                $.each(val[i], function(key, val) {
+                    if (market == key) {
+                        setParameters(val);
+                    }
+                });
+            };
+        });
+
+    });
 }
 
 
-function setParameters(data){
-	$.each( data, function( key, val ) {
-		if (key=='phone' && val!='') {
-			var element = document.getElementById("phone-number");
-			while (element.firstChild) {
-    				element.removeChild(element.firstChild);
-				}
-				element.appendChild(document.createTextNode(val));
-
-			var element = document.getElementById("phone-number-link");
-			element.setAttribute('href','tel:'+val)
-		}
-		if (key=='email' && val!='') {
-  				var element = document.getElementById("email");
-  				while (element.firstChild) {
-    				element.removeChild(element.firstChild);
-				}
-				element.appendChild(document.createTextNode(val));
-
-				var element = document.getElementById("email-link");
-				element.setAttribute('href','mailto:'+val)
-		}
-		if (key=='workingHours' && val!='') {
-  				var element = document.getElementById("working-hours");
-  				while (element.firstChild) {
-    				element.removeChild(element.firstChild);
-				}
-				element.appendChild(document.createTextNode('Mon-Fri '+val));
-				
-				var element = document.getElementById("weekend-hours");
-				element.innerHTML = 'Sat-Sun Limited Hours'
-		}
-		if (key=='sitename' && val!='') {
-			var element = document.getElementById("header-image");
-			element.setAttribute('src','http://www.gannett-cdn.com/sites/'+val+'/images/footer-logo.png')
-
-			var element = document.getElementById("sign-in");
-			element.setAttribute('href','https://account.'+val+'.com/personal-information')
-
-			var element = document.getElementById("member-guide");
-			element.setAttribute('href','http://'+val+'.com/memberguide')
-
-			var element = document.getElementById("e-newspaper");
-			element.setAttribute('href','https://account.'+val+'.com/enewspaper')
-
-			var element = document.getElementById("start-stop");
-			element.setAttribute('href','https://account.'+val+'.com/delivery-temporary-stop')
-
-			var element = document.getElementById("report-issue");
-			element.setAttribute('href','https://account.'+val+'.com/delivery-issue')
-
-			var element = document.getElementById("payment");
-			element.setAttribute('href','https://account.'+val+'.com/pay')
-
-			var element = document.getElementById("history");
-			element.setAttribute('href','https://account.'+val+'.com/history')
-
-		}
-		if (key=='chatUrl' && val!='') {
-			var element = document.getElementById("chat-url");
-			element.setAttribute('href',val)
-		}
-
-	});
+function setParameters(data) {
+    $.each(data, function(key, val) {
+        if (key == 'phone' && val != '') {
+            var element = document.getElementById("phone-number");
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+            element.appendChild(document.createTextNode(val));
+            element = document.getElementById("phone-number-link");
+            element.setAttribute('href', 'tel:' + val);
+        }
+        if (key == 'email' && val != '') {
+            var element = document.getElementById("email");
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+            element.appendChild(document.createTextNode(val));
+            element = document.getElementById("email-link");
+            element.setAttribute('href', 'mailto:' + val);
+        }
+        if (key == 'workingHours' && val != '') {
+            var element = document.getElementById("working-hours");
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+            element.appendChild(document.createTextNode('Mon-Fri ' + val));
+            element = document.getElementById("weekend-hours");
+            element.innerHTML = 'Sat-Sun Limited Hours';
+        }
+        if (key == 'sitename' && val != '') {
+            var element = document.getElementById("header-image");
+            // element.setAttribute('src', 'http://www.gannett-cdn.com/sites/' + val + '/images/footer-logo.png');
+            element.setAttribute('src', 'http://www.gannett-cdn.com/sites/' + val + '/images/footer-logo@2x.png' );
+            element = document.getElementById("sign-in");
+            element.setAttribute('href', 'https://account.' + val + '.com/personal-information');
+            element = document.getElementById("member-guide");
+            element.setAttribute('href', 'http://' + val + '.com/memberguide')
+            element = document.getElementById("e-newspaper");
+            element.setAttribute('href', 'https://account.' + val + '.com/enewspaper');
+            element = document.getElementById("start-stop");
+            element.setAttribute('href', 'https://account.' + val + '.com/delivery-temporary-stop');
+            element = document.getElementById("report-issue");
+            element.setAttribute('href', 'https://account.' + val + '.com/delivery-issue');
+            element = document.getElementById("payment");
+            element.setAttribute('href', 'https://account.' + val + '.com/pay');
+            element = document.getElementById("history");
+            element.setAttribute('href', 'https://account.' + val + '.com/history');
+        }
+        if (key == 'chatUrl' && val != '') {
+            var element = document.getElementById("chat-url");
+            if (val == 'NA') {
+                element.removeChild(element.firstChild);
+            } else {
+                element.setAttribute('href', val);
+            }
+        }
+    });
 }
-
-
