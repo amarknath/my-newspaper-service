092656e88a0390a70c308fd6ba4511cb9b66c020
Caley Dorsey <dorseycaley@gmail.com>
2016-09-15 15:49:37 -0400

    changes to/ preparing publication page for code review

--- js/publication.js
+++ js/publication.js
@@ -19,63 +19,64 @@ if (market != undefined) {
 function setParameters(data) {
     $.each(data, function(key, val) {
         if (key == 'phone' && val != '') {
-            var element = document.getElementById("phone-number");
+            var element = document.getElementById("mcis-phone-number");
             while (element.firstChild) {
                 element.removeChild(element.firstChild);
             }
             element.appendChild(document.createTextNode(val));
-            element = document.getElementById("phone-number-link");
+            element = document.getElementById("mcis-phone-number-link");
             element.setAttribute('href', 'tel:' + val);
         }
         if (key == 'email' && val != '') {
-            var element = document.getElementById("email");
+            var element = document.getElementById("mcis-email");
             while (element.firstChild) {
                 element.removeChild(element.firstChild);
             }
             element.appendChild(document.createTextNode(val));
-            element = document.getElementById("email-link");
+            element = document.getElementById("mcis-email-link");
             element.setAttribute('href', 'mailto:' + val);
         }
         if (key == 'workingHours' && val != '') {
-            var element = document.getElementById("working-hours");
+            var element = document.getElementById("mcis-working-hours");
             while (element.firstChild) {
                 element.removeChild(element.firstChild);
             }
             element.appendChild(document.createTextNode('Mon-Fri ' + val));
-            element = document.getElementById("weekend-hours");
+            element = document.getElementById("mcis-weekend-hours");
             element.innerHTML = 'Sat-Sun Limited Hours';
         }
         if (key == 'sitename' && val != '') {
-            var element = document.getElementById("header-image");
+            var element = document.getElementById("sh-header-image");
             var img = new Image();
+            img.className = 'sh-actual-pub-logo';
             img.src = 'http://www.gannett-cdn.com/sites/' + val + '/images/footer-logo.png';
             img.setAttribute('onerror',"this.onerror=null;this.src='http://www.gannett-cdn.com/sites/" + val + "/images/footer-logo@2x.png';" )
             
             element.appendChild(img);
 
-            element = document.getElementById("sign-in");
+            element = document.getElementById("pbm-sign-in");
             element.setAttribute('href', 'https://account.' + val + '.com/personal-information');
 
-            element = document.getElementById("member-guide");
+            element = document.getElementById("pbm-member-guide");
             element.setAttribute('href', 'http://' + val + '.com/memberguide');
 
-            element = document.getElementById("e-newspaper");
+            element = document.getElementById("pbm-e-newspaper");
             element.setAttribute('href', 'https://account.' + val + '.com/enewspaper');
 
-            element = document.getElementById("start-stop");
+            element = document.getElementById("pbm-start-stop");
             element.setAttribute('href', 'https://account.' + val + '.com/delivery-temporary-stop');
 
-            element = document.getElementById("report-issue");
+            element = document.getElementById("pbm-report-issue");
             element.setAttribute('href', 'https://account.' + val + '.com/delivery-issue');
 
-            element = document.getElementById("payment");
+            element = document.getElementById("pbm-payment");
             element.setAttribute('href', 'https://account.' + val + '.com/pay');
             
-            element = document.getElementById("history");
+            element = document.getElementById("pbm-history");
             element.setAttribute('href', 'https://account.' + val + '.com/history');
         }
         if (key == 'chatUrl' && val != '') {
-            var element = document.getElementById("chat-url");
+            var element = document.getElementById("pbm-chat-url");
             if (val == 'NA') {
                 element.removeChild(element.firstChild);
             } else {
