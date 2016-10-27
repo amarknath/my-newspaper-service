51fddbfee642b6bd4b7befe190a14ad7ff32f724
am@rn@th <amarnathk.k1@gmail.com>
2016-10-27 14:08:39 -0400

    chat links fixed

--- js/publication.js
+++ js/publication.js
@@ -26,6 +26,8 @@ function setParameters(data) {
             element.appendChild(document.createTextNode(val));
             element = document.getElementById("mcis-phone-number-link");
             element.setAttribute('href', 'tel:' + val);
+
+            
         }
         if (key == 'email' && val != '') {
             var element = document.getElementById("mcis-email");
@@ -42,37 +44,32 @@ function setParameters(data) {
                 element.removeChild(element.firstChild);
             }
             element.appendChild(document.createTextNode('Mon-Fri ' + val));
-            element = document.getElementById("mcis-weekend-hours");
+            var element = document.getElementById("mcis-weekend-hours");
             element.innerHTML = 'Sat-Sun Limited Hours';
         }
         if (key == 'sitename' && val != '') {
-            var element = document.getElementById("sh-header-image");
-            var img = new Image();
-            img.className = 'sh-actual-pub-logo';
-            img.src = 'http://www.gannett-cdn.com/sites/' + val + '/images/footer-logo.png';
-            img.setAttribute('onerror',"this.onerror=null;this.src='http://www.gannett-cdn.com/sites/" + val + "/images/footer-logo@2x.png';" )
-            
-            element.appendChild(img);
 
-            element = document.getElementById("pbm-sign-in");
+            siteName = val;
+
+            var element = document.getElementById("pbm-sign-in");
             element.setAttribute('href', 'https://account.' + val + '.com/personal-information');
 
-            element = document.getElementById("pbm-member-guide");
+            var element = document.getElementById("pbm-member-guide");
             element.setAttribute('href', 'http://' + val + '.com/memberguide');
 
-            element = document.getElementById("pbm-e-newspaper");
+            var element = document.getElementById("pbm-e-newspaper");
             element.setAttribute('href', 'https://account.' + val + '.com/enewspaper');
 
-            element = document.getElementById("pbm-start-stop");
+            var element = document.getElementById("pbm-start-stop");
             element.setAttribute('href', 'https://account.' + val + '.com/delivery-temporary-stop');
 
-            element = document.getElementById("pbm-report-issue");
+            var element = document.getElementById("pbm-report-issue");
             element.setAttribute('href', 'https://account.' + val + '.com/delivery-issue');
 
-            element = document.getElementById("pbm-payment");
+            var element = document.getElementById("pbm-payment");
             element.setAttribute('href', 'https://account.' + val + '.com/pay');
             
-            element = document.getElementById("pbm-history");
+            var element = document.getElementById("pbm-history");
             element.setAttribute('href', 'https://account.' + val + '.com/history');
         }
         if (key == 'chatUrl' && val != '') {
@@ -82,6 +79,22 @@ function setParameters(data) {
             } else {
                 element.setAttribute('href', val);
             }
+        }
+        if (key == 'use2xLogo' && val != '') {
+            var element = document.getElementById("sh-header-image");
+            var img = new Image();
+            img.className = 'sh-actual-pub-logo';
+            if (val == 'true')
+                img.src = 'http://www.gannett-cdn.com/sites/' + siteName + '/images/footer-logo@2x.png';
+            else {
+                img.src = 'http://www.gannett-cdn.com/sites/' + siteName + '/images/footer-logo.png';
+            }            
+            element.appendChild(img);
+
+        }
+        if (key == 'weekendHours' && val != '') {
+            var element = document.getElementById("mcis-weekend-hours");
+            element.innerHTML = 'Sat-Sun ' + val;
         }
     });
 }
